75c9cfad10edb6d454889b78d650353373c037ff
Joseph Castle <joseph.castle@gsa.gov>
2020-08-12 15:52:02 -0400

    Parts 1, 2, and 3. Starting clean with repo by part.

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,16 +1,38 @@
 import React from "react"
-// highlight-next-line
 import styles from "./about-css-modules.module.css"
 import Container from "../components/container"
 
-// highlight-next-line
 console.log(styles)
+
+// highlight-start
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+// highlight-end
 
 export default function About() {
   return (
     <Container>
       <h1>About CSS Modules</h1>
       <p>CSS Modules are cool</p>
+      {/* highlight-start */}
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      {/* highlight-end */}
     </Container>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react"
-import { Link } from "gatsby" // highlight-line
-import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link> {/* highlight-line */}
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    // highlight-next-line
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
     </div>
   )
 }
