345823110f2063c0fcb4e138e1617db4bb417a50
Joseph Castle <joseph.castle@gsa.gov>
2020-08-11 21:49:56 -0400

    Post initialization with updated files from tutorial.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby" // highlight-line
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link> {/* highlight-line */}
+      <Header headerText="Hello Gatsby!" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
